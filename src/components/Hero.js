70fd4502dca1633f068c0e7f30dfa0ab1bc1c6c7
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-17 01:17:12 +0530

    Added contact info

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,11 @@ const Hero = () => {
     <div className='main'>
       <h1>Hi, Welcome to Jhill's art and crafts</h1>
       <h2>Paintings coming soon...</h2>
+      <div style={{textAlign: 'center'}}>
+        <div style={{marginBottom: 6}}><b>FOR ORDERS:</b></div>
+        <div><b style={{paddingRight: 6}}>Mail me at: </b>jhillsartandcrafts@gmail.com</div>
+        <div><b style={{paddingRight: 6}}>Contact: </b>+91 9401885232</div>
+      </div>
     </div>
   );
 };
