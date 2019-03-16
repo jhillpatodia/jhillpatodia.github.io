2442f1302d38b66e10003b80683fd91815bb4526
Jhill Patodia <48619613+jhillpatodia@users.noreply.github.com>
2019-03-16 22:46:04 +0530

    Creating build

---
+++ precache-manifest.ed9ba0da204adb9c5a605e662bc75681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bde44c9b9c93ce9dab38",
+    "url": "/static/js/main.bec9ad14.chunk.js"
+  },
+  {
+    "revision": "4231adcdc479f41c33cb",
+    "url": "/static/js/2.4947ba8c.chunk.js"
+  },
+  {
+    "revision": "bde44c9b9c93ce9dab38",
+    "url": "/static/css/main.4fc9229f.chunk.css"
+  },
+  {
+    "revision": "2ad9cc0661bc9a714883e246cda0a722",
+    "url": "/index.html"
+  }
+];
