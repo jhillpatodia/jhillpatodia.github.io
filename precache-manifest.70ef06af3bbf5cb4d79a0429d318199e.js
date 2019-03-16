b71b832fd18e157bc4ed199e048a1e43a6601a53
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-17 01:19:59 +0530

    Updates

--- precache-manifest.70ef06af3bbf5cb4d79a0429d318199e.js
+++ precache-manifest.70ef06af3bbf5cb4d79a0429d318199e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/home/static/js/runtime~main.0990ee84.js"
   },
   {
-    "revision": "b69e6ac9f653c0600c13",
-    "url": "/home/static/js/main.bec9ad14.chunk.js"
+    "revision": "cd107b0d84758fdf305e",
+    "url": "/home/static/js/main.5658c652.chunk.js"
   },
   {
     "revision": "4231adcdc479f41c33cb",
     "url": "/home/static/js/2.4947ba8c.chunk.js"
   },
   {
-    "revision": "b69e6ac9f653c0600c13",
+    "revision": "cd107b0d84758fdf305e",
     "url": "/home/static/css/main.98415780.chunk.css"
   },
   {
-    "revision": "ea89006aa8a3c8368052cad802f9f0e8",
+    "revision": "2429d08ff87d1ad746d3ff42b26c9822",
     "url": "/home/index.html"
   }
 ];
