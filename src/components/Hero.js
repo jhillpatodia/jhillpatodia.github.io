bf32764d9e243560abf1bc517e19d5903d601ec8
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-16 22:36:28 +0530

    Build

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='main'>
+      <h1>Hi, Welcome to Jhill's art and crafts</h1>
+      <h2>Paintings coming soon...</h2>
+    </div>
+  );
+};
+
+export default Hero;
