1225929373ea7bafa73736f9b18f19fc3a3f77fd
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-17 08:43:36 +0530

    Build production

---
+++ precache-manifest.eaea1319ec926230cd3fc94d6f083036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "845bb8d64374aea49362",
+    "url": "/static/js/main.b7f691a4.chunk.js"
+  },
+  {
+    "revision": "4231adcdc479f41c33cb",
+    "url": "/static/js/2.4947ba8c.chunk.js"
+  },
+  {
+    "revision": "845bb8d64374aea49362",
+    "url": "/static/css/main.98415780.chunk.css"
+  },
+  {
+    "revision": "e9a8e6fb15edb27335abece9ca9cbbec",
+    "url": "/index.html"
+  }
+];
