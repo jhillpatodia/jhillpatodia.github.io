24dfbd6bbe310ce3511e91388b036c2f873e7683
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-17 08:32:24 +0530

    Making dev and master branch even

---
+++ README.md
@@ -0,0 +1,1 @@
+# jhillpatodia.github.io
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from "./Header";
+import Hero from "./Hero";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      {/*<Header />*/}
+      <Hero />
+    </React.Fragment>
+  );
+};
+
+export default App;
