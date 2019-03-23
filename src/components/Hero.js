81ddc9df5f2142559fa81b246abb01be959f439a
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-23 20:29:16 +0530

    Made landing page awesome

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='main' style={{textAlign: 'center'}}>
-      <h1>Hi, Welcome to Jhill's art and crafts</h1>
-      <h2>Paintings coming soon...</h2>
-      <div style={{textAlign: 'center'}}>
-        <div style={{marginBottom: 6}}><b>FOR ORDERS:</b></div>
-        <div><b style={{paddingRight: 6}}>Mail me at: </b>jhillsartandcrafts<span style={{fontFamily: 'sans-serif', fontSize: 12}}>@</span>gmail.com</div>
-        <div><b style={{paddingRight: 6}}>Contact: </b>+91 9101700198</div>
+    <div className='main'>
+      <div className='main-background' />
+      <div className='main-content'>
+        <h1>Hi,<br/>Welcome to<br/>Jhill's art and crafts</h1>
+        <h3>Paintings coming soon...</h3>
+        <div className='main-orders'>
+          <div className='mb-8'><b>FOR ORDERS:</b></div>
+          <div className='mb-6'><b style={{paddingRight: 6}}>Mail me at: </b>jhillsartandcrafts@gmail.com</div>
+          <div><b style={{paddingRight: 6}}>Contact: </b>+91 9101700198</div>
+        </div>
       </div>
     </div>
   );
