45770748534e9c6b18fc6cfada9d225f69855ae1
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-03-24 12:17:03 +0530

    Reducing page load size, removing font-awesome

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,14 +10,14 @@ const Hero = () => {
         <div className='main-orders'>
           <div className='order'>
             <div className='order-title'>ORDER SKETCH</div>
-            <i className="fa fa-chevron-circle-down" aria-hidden="true" />
+            <div className='icons arrow-down'><img src={require('../assets/arrow_down.png')} alt='' /></div>
           </div>
           <div className='contact'>
-            <span className='f-icon mr-8'><i className="fa fa-envelope" aria-hidden="true" /></span>
+            <div className='icons mr-8'><img src={require('../assets/mail.png')} alt='' /></div>
             <a href="mailto:jhillsartandcrafts@gmail.com?subject=Placing an order online ">jhillsartandcrafts@gmail.com</a>
           </div>
           <div className='contact'>
-            <span className='f-icon mr-8'><i className="fa fa-mobile f-mobile" aria-hidden="true" /></span>
+            <div className='icons mr-8'><img src={require('../assets/phone.png')} alt='' /></div>
             <a href="tel:+91 9101700198">+91 9101700198</a>
           </div>
         </div>
