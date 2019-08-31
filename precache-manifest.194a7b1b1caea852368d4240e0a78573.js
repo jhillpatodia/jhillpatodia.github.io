1abd2d4e36ce98a175f217e9b57744dd5db194ed
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-09-01 01:40:23 +0530

    Add gallery and lot of code refactor

---
+++ precache-manifest.194a7b1b1caea852368d4240e0a78573.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae73b160a3b4aa5b38a5",
+    "url": "/static/css/main.e98275be.chunk.css"
+  },
+  {
+    "revision": "ae73b160a3b4aa5b38a5",
+    "url": "/static/js/main.3fcb2fc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a21969824921be96893",
+    "url": "/static/css/2.0c008a29.chunk.css"
+  },
+  {
+    "revision": "5a21969824921be96893",
+    "url": "/static/js/2.89e3325e.chunk.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "42352dfc830221e692319ac4cc427068",
+    "url": "/static/media/bg_building_1.42352dfc.png"
+  },
+  {
+    "revision": "bcea2467b09670f45ae48d6f1fab3582",
+    "url": "/index.html"
+  }
+];
